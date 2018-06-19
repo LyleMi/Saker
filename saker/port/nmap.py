f42d1b750085ce77cd8f2fa904af017497cf9a91
lyle <lilyui@126.com>
2018-06-19 10:32:43 +0800

    add nmap scan api

--- saker/port/nmap.py
+++ saker/port/nmap.py
@@ -1,10 +1,80 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-from json import dumps
+import os
+import json
+import subprocess
+import xml.etree.cElementTree as ET
 
 
 class Nmap(object):
 
-    def __init__(self):
-        pass
+    args = ['-sV', '-sT', '-Pn']
+    ports = '21-25,80-89,110,111,143,443,513,873,1080,1158,1433,1521,2049,2181,3306-3308,3389,3690,5900,6370-6379,7001,8000-8090,8161,9000,9418,11211,27017-27019,50060'
+    tmpName = 'tmp-nmap.xml'
+
+    def __init__(self, target, options=[]):
+        self.target = target
+        self.options = options
+
+    def run(self):
+        cmd = ["nmap"]
+        cmd += self.args + self.options
+        cmd += ["-p", self.ports, self.target]
+        cmd.extend(["-oX", self.tmpName])
+        ret = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        out, error = ret.communicate()
+        if error:
+            print(error)
+            return False
+        self.ret = self.parse_nmap_xml()
+        return True
+
+    def parse_nmap_xml(self):
+        """
+        Parse the xml file and extract the imforation of services.
+        :return: dict. Use open port number as key and concrete infomation of the web service as value.
+        """
+        try:
+            tree = ET.parse(self.tmpName)
+            os.remove(self.tmpName)
+            root = tree.getroot()
+            result = {}
+            filter_flag = True
+            for port in root.find('host').find('ports').findall('port'):
+                if port.find('state').get('state') not in ('filtered', 'closed'):
+                    filter_flag = False
+                if port.find('state').get('state') == 'open':
+                    service = port.find('service').attrib
+                    service.pop('conf')
+                    service.pop('method')
+                    result[port.get('portid')] = service
+        except Exception as e:
+            print(e)
+            return None
+
+        # What if we get nothing from the xml...
+        if not result:
+            if filter_flag:
+                print('All open ports detected by zmap are actually filtered or closed!')
+            else:
+                print('Failed to parse nmap xml!')
+            return None
+
+        return result
+
+
+if __name__ == '__main__':
+    import time
+    ip = "localhost"
+    content = ""
+    with open("domain.txt", "rb") as fh:
+        content = fh.read()
+    for domain in content.split("\n"):
+        print("[%s] try %s now..." % (time.time(), domain))
+        break
+        n = Nmap(domain)
+        ret = n.run()
+        if ret:
+            with open(domain, "wb") as fh:
+                fh.write(json.dumps(n.ret))
