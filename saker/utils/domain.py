6a403bac1ec53866fc1b04ed471d7f7abc0ac88e
lyle <lilyui@126.com>
2018-06-27 22:12:12 +0800

    update cidr

--- saker/utils/domain.py
+++ saker/utils/domain.py
@@ -4,6 +4,8 @@
 import re
 import socket
 
+from netaddr import IPRange, IPNetwork
+
 
 class RES:
     ip = re.compile(r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$")
@@ -12,21 +14,73 @@ class RES:
     v6range = re.compile(r"[0-9a-fA-F]+:[0-9A-Fa-f:.]+\-[0-9a-fA-F]+:[0-9A-Fa-f:.]+$")
     glob = re.compile(r"\d+\.\d+\.\d+\.\*$")
     bracket = re.compile(r"(.*?)\.(\d+)[\[\{\(](.*)[\)\}\]]$")  # parses '1.2.3.4[5-9]' or '1.2.3.[57]'
-    hyphen = re.compile(r"(.*?)\.(\d+)\-(\d+)$")
+    crange = re.compile(r"(.*?)\.(\d+)\-(\d+)$")
+    brange = re.compile(r"(.*?)\.(\d+)\-(\d+).(\d+)$")
     hostname = re.compile(r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?)', re.IGNORECASE)
 
 
 def cidrize(ipstr):
-    # TODO
+    ipstr = ipstr.replace(' ', '').strip()
+
     if isIPv4(ipstr):
         return [ipstr]
+
     ips = []
     if ',' in ipstr:
         for ip in ipstr.split(","):
             ips.extend(cidrize(ip))
         return list(set(ips))
+
+    if RES.cidr.match(ipstr):
+        return [str(i) for i in IPNetwork(ipstr)]
+
     if RES.v4range.match(ipstr):
+        start, finish = ipstr.split('-')
+        return [str(i) for i in IPRange(start, finish)]
+
+    if RES.crange.match(ipstr):
+        # c段
+        start, finish = ipstr.split('-')
+        finish = ".".join(start.split(".")[:-1] + [finish])
+        return [str(i) for i in IPRange(start, finish)]
+
+    if RES.brange.match(ipstr):
+        # b段
+        parts = ipstr.split('.')
+        temp = parts[2].split("-")
+        for i in range(int(temp[0]), int(temp[1])+1):
+            parts[2] = str(i)
+            ips.append(".".join(parts))
         return ips
+
+    if RES.bracket.match(ipstr):
+        match = RES.bracket.match(ipstr)
+        parts = match.groups()
+
+        if len(parts) == 3:
+            prefix, subnet, enders = parts
+            network = '.'.join((prefix, subnet))
+
+        # '1.2.3.[5-9] style
+        elif len(parts) == 2:
+            prefix, enders = parts
+            network = prefix + '.'
+
+        # Split hyphenated [x-y]
+        if '-' in enders:
+            first, last = enders.split('-')
+
+        # Get first/last from [xy] - This really only works with single
+        # digits
+        elif len(enders) >= 2:
+            # Creating a set and sorting to ensure that [987] won't throw
+            # an exception. Might be too inclusive, but screw it.
+            uniques = sorted(set(enders))
+            first = uniques[0]
+            last = uniques[-1]
+
+        return [str(i) for i in IPRange(network + first, network + last)]
+
     return ips
 
 
