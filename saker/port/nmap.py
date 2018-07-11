880270090a734755dded93cfebb80bb50d5e5e17
lyle <lilyui@126.com>
2018-07-11 08:09:25 +0800

    update nmap

--- saker/port/nmap.py
+++ saker/port/nmap.py
@@ -3,6 +3,7 @@
 
 import os
 import json
+import uuid
 import subprocess
 import xml.etree.cElementTree as ET
 
@@ -11,7 +12,6 @@ class Nmap(object):
 
     args = ['-sV', '-sT', '-Pn']
     ports = '21-25,80-89,110,111,143,443,513,873,1080,1158,1433,1521,2049,2181,3306-3308,3389,3690,5900,6370-6379,7001,8000-8090,8161,9000,9418,11211,27017-27019,50060'
-    tmpName = 'tmp-nmap.xml'
 
     def __init__(self, target, options=[]):
         self.target = target
@@ -21,15 +21,16 @@ class Nmap(object):
         cmd = ["nmap"]
         cmd += self.args + self.options
         cmd += ["-p", self.ports, self.target]
-        cmd.extend(["-oX", self.tmpName])
+        tmpName = '.tmp-nmap.%s.xml' % uuid.uuid4().hex
+        cmd.extend(["-oX", tmpName])
         ret = subprocess.Popen(cmd, stdout=subprocess.PIPE)
         out, error = ret.communicate()
         if error:
             print(error)
             return False
 
-        tree = ET.parse(self.tmpName)
-        os.remove(self.tmpName)
+        tree = ET.parse(tmpName)
+        os.remove(tmpName)
         root = tree.getroot()
         result = {}
         filter_flag = True
@@ -58,8 +59,8 @@ class Nmap(object):
             else:
                 print('Failed to parse nmap xml!')
             return None
-
         return result
+
 
 
 if __name__ == '__main__':
