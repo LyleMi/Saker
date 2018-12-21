ce11735370fde583476f39ad7cd0aa08847b7b47
lyle <lilyui@126.com>
2018-12-21 18:54:06 +0800

    update release

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ requires = [
 
 setup(
     name="Saker",
-    version="1.0.1",
+    version="1.0.4",
     keywords=("Web Security", "Fuzz"),
     description="Tool For Fuzz Web Applications",
     long_description=long_description,
