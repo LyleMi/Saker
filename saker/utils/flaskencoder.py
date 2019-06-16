49e995e8f207d3bbab95019f3d5703e366e99255
lyle <lilyui@126.com>
2019-06-16 09:06:15 +0800

    update: use flask session api to encode / decode

--- saker/utils/flaskencoder.py
+++ saker/utils/flaskencoder.py
@@ -1,49 +1,46 @@
 
 import zlib
 import json
-import hashlib
-from itsdangerous import base64_decode, base64_encode
-from itsdangerous import URLSafeTimedSerializer
-from flask.json.tag import TaggedJSONSerializer
 
-session_json_serializer = TaggedJSONSerializer()
+from itsdangerous import base64_decode, base64_encode
+
+from flask import Flask
+from flask.sessions import SecureCookieSessionInterface
+
 
 class FlaskEncoder(object):
 
     salt = "cookie-session"
-    key_derivation = "hmac"
-    digest_method = staticmethod(hashlib.sha1)
-    serializer = session_json_serializer
 
-    def __init__(self):
+    def __init__(self, secret_key=None):
         self.compressed = False
+        if secret_key is None:
+            self.session_serializer = None
+        else:
+            app = Flask(__name__)
+            app.secret_key = secret_key
+            self.session_serializer = SecureCookieSessionInterface().get_signing_serializer(app)
 
     def decode(self, cookie):
-        payload = cookie
-        if payload.startswith('.'):
-            self.compressed = True
-            payload = payload[1:]
-        data = payload.split(".")[0]
-        data = base64_decode(data)
-        if self.compressed:
-            data = zlib.decompress(data)
-        return json.loads(data.decode("utf-8"))
+        if self.session_serializer is None:
+            payload = cookie
+            if payload.startswith('.'):
+                self.compressed = True
+                payload = payload[1:]
+            data = payload.split(".")[0]
+            data = base64_decode(data)
+            if self.compressed:
+                data = zlib.decompress(data)
+            return json.loads(data.decode("utf-8"))
+        else:
+            return self.session_serializer.loads(cookie)
 
     def encode(self, cookie, secret_key=None):
-        if secret_key is None:
+        if self.session_serializer is None:
             if self.compressed:
                 cookie = "." + base64_encode(zlib.compress(cookie))
             else:
                 cookie = base64_encode(cookie)
             return cookie
-        signer_kwargs = dict(
-            key_derivation=self.key_derivation, digest_method=self.digest_method
-        )
-        serializer = URLSafeTimedSerializer(
-            secret_key,
-            salt=self.salt,
-            serializer=self.serializer,
-            signer_kwargs=signer_kwargs,
-        )
-        return serializer.dumps(cookie)
-
+        else:
+            return self.session_serializer.dumps(cookie)
